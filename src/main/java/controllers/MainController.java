7348843db323b991b4691ed02454bd8024050d9c
DevelopAndrAhA <j.altynbek@bk.ru>
2023-04-09 22:18:09 +0300

    8-й апрель.Исправлена карта, добавлено удаление

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -216,14 +216,14 @@ public class MainController {
 
 	@ResponseBody
 	@RequestMapping(value = "delete",method = RequestMethod.GET)
-	public Object delete(@RequestParam("deviceId")String deviceId,@RequestParam("faceFeatures_id")String faceFeatures_id){
+	public Object delete(@RequestParam("deviceId")String deviceId,@RequestParam("faceFeatures_id")String faceFeatures_id,@RequestParam("fullFaceFeatures_id")String fullFaceFeatures_id){
 		boolean res = service.deleteFace(deviceId,faceFeatures_id);
-		int tmpFaceFeatures_id = Integer.parseInt(faceFeatures_id);
-		/*if(res){
+		int tmpFullFaceFeatures_id = Integer.parseInt(fullFaceFeatures_id);
+		if(res){
 			Iterator<FullFaceFeatures> iterator = fullFaceFeatures.iterator();
 			while (iterator.hasNext()) {
 				FullFaceFeatures fullFaceFeature = iterator.next();
-				if (fullFaceFeature.getDeviceId().equals(deviceId) && fullFaceFeature.getFaceFeatures(1).getFaceFeatures_id()==tmpFaceFeatures_id) {
+				if (fullFaceFeature.getDeviceId().equals(deviceId) && fullFaceFeature.getFullFaceFeatures_id()==tmpFullFaceFeatures_id) {
 					File file = new File(FileUtils.getUserDirectory()+File.separator+"images"+File.separator+fullFaceFeature.getPhotoName()+".jpg");
 					file.delete();
 					file = new File(FileUtils.getUserDirectory()+File.separator+"images"+File.separator+fullFaceFeature.getPhotoName()+"_SMALL.jpg");
@@ -231,18 +231,8 @@ public class MainController {
 					iterator.remove();
 				}
 			}
-		}*/
-		Iterator<FullFaceFeatures> iterator = fullFaceFeatures.iterator();
-		while (iterator.hasNext()) {
-			FullFaceFeatures fullFaceFeature = iterator.next();
-			if (fullFaceFeature.getDeviceId().equals(deviceId) && fullFaceFeature.getFaceFeatures(1).getFaceFeatures_id()==tmpFaceFeatures_id) {
-				File file = new File(FileUtils.getUserDirectory()+File.separator+"images"+File.separator+fullFaceFeature.getPhotoName()+".jpg");
-				file.delete();
-				file = new File(FileUtils.getUserDirectory()+File.separator+"images"+File.separator+fullFaceFeature.getPhotoName()+"_SMALL.jpg");
-				file.delete();
-				iterator.remove();
-			}
 		}
+
 		return "{'status':200}";
 	}
 
