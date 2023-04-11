209c557fc5772e76e7c3c38d6804adb2ec9bf969
DevelopAndrAhA <j.altynbek@bk.ru>
2023-04-11 21:15:12 +0300

    8-й апрель.Исправлена карта, добавлено удаление.Добавлен поиск по Косинусу

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -362,7 +362,7 @@ public class MainController {
 		norm2 = Math.sqrt(norm2);
 		cosDistance = cosDistance / (norm1 * norm2);
 
-		if (cosDistance >= 0.9) {
+		if (cosDistance >= 0.5) {
 			prediction = new Prediction((float) cosDistance, key, 0, photoName);
 			return prediction;
 		}
